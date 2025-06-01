783e8543845ee6ad1609c58bbd8cce4a5408aff1
Torna <tornaenri@gmail.com>
2025-06-01 15:19:38 +0200

    Minor updates

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # E2EE_Chat
-university project for "Sicurezza dell'Informazione M": end-to-end encrypted communication in java
+University project for "Sicurezza dell'Informazione M": end-to-end encrypted communication in java
---
+++ e2ee_chat/src/utils/ConvertingUtils.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.util.HexFormat;
+
+public class ConvertingUtils {
+
+    /*
+     * Method to convert a String into a byte array.
+     */
+    public static byte[] toByteArray(String input) {
+        return input.getBytes();
+    }
+
+    /*
+     * Method to convert a byte array into a hex string.
+     */
+    public static String toHexString(byte[] input) {
+        return HexFormat.of().formatHex(input);
+    }
+
+    /*
+     * Method to convert a hex string into a byte array.
+     */
+    public static byte[] fromHexString(String input) {
+        return HexFormat.of().parseHex(input);
+    }
+
+}
